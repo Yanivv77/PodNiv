116fd9b53a5eef7dec5e3b2cf543eff1f8bc7279
Yaniv <Yanivv77@gmail.com>
2024-06-03 22:55:17 +0300

    Add mobile nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
 import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
 
@@ -14,16 +15,17 @@ export default function RootLayout({
         
         <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
           <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
-            <div className="flex h-16 items-center justify-between md:hidden">
+          <div className="flex h-16 items-center justify-between md:hidden">
               <Image 
-                src="/icons/logo.svg"
+                src="/icons/podniv-icon.svg"
                 width={30}
                 height={30}
                 alt="menu icon"
               />
-              
+              <MobileNav/>
             </div>
             <div className="flex flex-col md:pb-14">
+              
               {children}
             </div>
           </div>
