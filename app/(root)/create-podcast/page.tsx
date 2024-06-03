69317a3aa26bd616cee1f525be72fc731cc76fa8
Yaniv <Yanivv77@gmail.com>
2024-06-03 18:54:52 +0300

    Add layout

---
+++ app/(root)/create-podcast/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreatePodcast = () => {
+  return (
+    <div>create</div>
+  )
+}
+
+export default CreatePodcast
---
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PodcastDetails = () => {
+  return (
+    <div>podcast</div>
+  )
+}
+
+export default PodcastDetails
