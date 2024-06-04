ef73fe61fada5adea08db58f4ddddc0c99c01232
Yaniv <Yanivv77@gmail.com>
2024-06-04 15:16:59 +0300

    Add cleck auth

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://honest-iguana-98.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
