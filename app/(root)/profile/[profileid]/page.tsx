2c261690dd2198c6c7811f7ea9a01995b1c46fda
Yaniv <Yanivv77@gmail.com>
2024-06-06 04:01:19 +0300

    Fix to profile page

--- app/(root)/profile/[profileid]/page.tsx
+++ app/(root)/profile/[profileid]/page.tsx
@@ -22,44 +22,41 @@ const ProfilePage = ({
     authorId: params.profileId,
   });
 
-  // Handle loading state more explicitly
-  if (!user || !podcastsData) {
-    return <LoaderSpinner />;
-  }
+  if (!user || !podcastsData) return <LoaderSpinner />;
 
   return (
     <section className="mt-9 flex flex-col">
       <h1 className="text-20 font-bold text-white-1 max-md:text-center">
-        Podcaster Profile
+      Podcaster Profile
       </h1>
       <div className="mt-6 flex flex-col gap-6 max-md:items-center md:flex-row">
-        {user && (
-          <ProfileCard
-            podcastData={podcastsData}
-            imageUrl={user.imageUrl}
-            userFirstName={user.name}
-          />
-        )}
+        <ProfileCard
+          podcastData={podcastsData!}
+          imageUrl={user?.imageUrl!}
+          userFirstName={user?.name!}
+        />
       </div>
       <section className="mt-9 flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">All Podcasts</h1>
         {podcastsData && podcastsData.podcasts.length > 0 ? (
           <div className="podcast_grid">
-            {podcastsData.podcasts.map((podcast) => (
-              <PodcastCard
-                key={podcast._id}
-                imgUrl={podcast.imageUrl}
-                title={podcast.podcastTitle}
-                description={podcast.podcastDescription}
-                podcastId={podcast._id}
-              />
-            ))}
+            {podcastsData?.podcasts
+              ?.slice(0, 4)
+              .map((podcast) => (
+                <PodcastCard
+                  key={podcast._id}
+                  imgUrl={podcast.imageUrl!}
+                  title={podcast.podcastTitle!}
+                  description={podcast.podcastDescription}
+                  podcastId={podcast._id}
+                />
+              ))}
           </div>
         ) : (
           <EmptyState
-            title="No podcasts found"
+            title="You have not created any podcasts yet"
             buttonLink="/create-podcast"
-            buttonLabel="Create Podcast"
+            buttonText="Create Podcast"
           />
         )}
       </section>
